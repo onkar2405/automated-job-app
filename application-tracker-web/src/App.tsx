420a4dc5da90b6b28ad44289a4f42b003edf620e
onkar2405 <onkarvaidyad789@gmail.com>
2025-08-14 01:45:05 +0530

    Removed unused class

--- application-tracker-web/src/App.tsx
+++ application-tracker-web/src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Job Application tracker</header>
+    <div>
+      <header>Job Application tracker</header>
     </div>
   );
 }
